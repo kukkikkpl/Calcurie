e0ff23fbb5a0487a74b8a6942cddfc37fbca3f30
Patcharaphorn Lervirul <kukkik.kpl@gmail.com>
2018-11-12 14:53:44 +0700

    Create login function using Firebase

---
+++ app/src/main/java/com/calcurie/calcurie/AppUtils.java
@@ -0,0 +1,19 @@
+package com.calcurie.calcurie;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class AppUtils {
+    Context context;
+
+    public AppUtils(Context context){
+        this.context = context;
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+}

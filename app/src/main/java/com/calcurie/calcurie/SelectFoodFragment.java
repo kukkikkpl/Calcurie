257ca29162337f91b0ab1b505908096b3580535b
panudet40 <59070130@it.kmitl.ac.th>
2018-11-13 18:50:29 +0700

    initial commit for Select_Food branch

---
+++ app/src/main/java/com/calcurie/calcurie/SelectFoodFragment.java
@@ -0,0 +1,24 @@
+package com.calcurie.calcurie;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SelectFoodFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_select_food, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        
+
+    }
+}

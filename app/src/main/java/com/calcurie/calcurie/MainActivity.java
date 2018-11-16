136887541b23239ee3972798746ffe5d7f0290e9
Patcharaphorn Lervirul <kukkik.kpl@gmail.com>
2018-11-17 03:14:27 +0700

    Show and edit profile

--- app/src/main/java/com/calcurie/calcurie/MainActivity.java
+++ app/src/main/java/com/calcurie/calcurie/MainActivity.java
@@ -16,8 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        bottomNavigationView = findViewById(R.id.navigation);
         setSupportActionBar(toolbar);
+
+        bottomNavigationView = findViewById(R.id.navigation);
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
